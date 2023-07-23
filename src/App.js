0bf68902b453d6cc9d0bcbfb36c175e4a0258796
Paola Arrieta <vargasp437@gmail.com>
2023-07-23 09:44:34 -0600

    add Formulario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import {Routes, Route } from "react-router-dom";
 import { Login } from "./Components/Login";
 import { Home } from "./Components/Home";
+import { FormularioV } from "./Components/FormularioV";
+import { Nav } from "./Components/Nav";
 
 function App() {
   return (
     <>
+    <Nav/>
       <Routes>
         <Route path="/Sesion" element={<Login/>}></Route>
         <Route path="/Inicio" element={<Home></Home>}></Route>
+        <Route path="/Ventas" element={<FormularioV/>}></Route>
       </Routes>    
     </>
   );
